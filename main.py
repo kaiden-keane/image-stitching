14741dc750f53de9e5aa8b5003815144977ce3df
Kaiden Keane <kjkeane88@gmail.com>
2024-10-01 22:35:44 -0600

    made it a bit faster by not sorting entire list, just find top x amnt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import math
+import os
 
 class Vec2d():
     def __init__(self, x:int=0 , y:int=0):
@@ -10,7 +11,18 @@ class Vec2d():
     def __str__(self):
         return f"({self.x}, {self.y})"
 
-def get_translation(matches: list[cv.DMatch]) -> tuple[int, int]:
+def get_top_features(amount_features, arr):
+    assert amount_features < len(arr)
+    top_arr = []
+    for i in range(amount_features):
+        lowest_val = arr[0]
+        for j in range(1, len(arr)):
+            if arr[i].distance < lowest_val.distance:
+                lowest_val = arr[i]
+        top_arr.append(lowest_val)
+    return top_arr
+
+def get_translation(top_amount, matches: list[cv.DMatch]) -> tuple[int, int]:
     """
     input: a list of cv.DMatch
     returns a tuple, (x, y) for translation that needs to occure to overlay the image
@@ -18,7 +30,7 @@ def get_translation(matches: list[cv.DMatch]) -> tuple[int, int]:
     translation = Vec2d()
     amnt = 0
     for match in matches:
-        if amnt > 10:
+        if amnt > top_amount:
             break
         translation.x += kp1[match.queryIdx].pt[0] - kp2[match.trainIdx].pt[0]
         translation.y += kp1[match.queryIdx].pt[1] - kp2[match.trainIdx].pt[1]
@@ -29,24 +41,31 @@ def get_translation(matches: list[cv.DMatch]) -> tuple[int, int]:
     
     return (int(math.ceil(translation.x)), int(math.ceil(translation.y)))
 
+img1 = cv.imread(os.path.join("sample_images", "left.png")) # queryImage / base image
+img2 = cv.imread(os.path.join("sample_images", "right.png")) # trainImage / translated image
 
-img1 = cv.imread('sample_images/left.png') # queryImage / base image
-img2 = cv.imread('sample_images/right.png') # trainImage / translated image
-
+# make the orb feature extractor
 orb = cv.ORB_create()
+
 # find the keypoints and descriptors with ORB
 kp1, des1 = orb.detectAndCompute(img1,None)
 kp2, des2 = orb.detectAndCompute(img2,None)
 
 # create BFMatcher object
 bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+
 # Match descriptors.
 matches = bf.match(des1,des2)
+
 # Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
+# matches = sorted(matches, key = lambda x:x.distance)
 
-translation = get_translation(matches)
+top_matches = get_top_features(10, matches)
 
+# gets the position img2 moves to match with img1
+translation = get_translation(10, top_matches)
+
+# get the offsets for where each image is
 img1_offset = Vec2d()
 img2_offset = Vec2d()
 if translation[0] < 0:
@@ -63,13 +82,13 @@ else:
     img1_offset.y = 0
 
 # get bounding box of both images after translation
+# blank_image is the array for the final, combined image
 blank_width = max(img1.shape[1], img2.shape[1] + translation[0]) - min(0, translation[0])
 blank_height = max(img1.shape[0], img2.shape[0] + translation[1]) - min(0, translation[1])
-
 blank_image_dimensions = Vec2d(blank_width, blank_height)
-
 blank_image = np.zeros((blank_image_dimensions.y, blank_image_dimensions.x, 3), np.uint8)
 
+# putting images on top of one another, no blending
 blank_image[img2_offset.y:img2_offset.y + img2.shape[0], img2_offset.x:img2_offset.x + img2.shape[1]] = img2
 blank_image[img1_offset.y:img1_offset.y + img1.shape[0], img1_offset.x:img1_offset.x + img1.shape[1]] = img1
 
