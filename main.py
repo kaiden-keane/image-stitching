5687e0b6253dab8a06ca62d1c076ffe40c0703f7
Kaiden Keane <kjkeane88@gmail.com>
2024-10-11 01:26:32 -0600

    works in bulk.. until the match fails

--- main.py
+++ main.py
@@ -24,18 +24,29 @@ class BaseImage():
     contains the data pertaining to one image
     key points, features, size, etc
     """
-    def __init__(self, filename): 
-        if (os.path.exists(filename)):
-            self.image = cv.imread(filename)
-        else:
-            raise FileNotFoundError
+    def __init__(self, data): 
+        self.image = data
         self.offset = Vec2d()
+        self.translation = Vec2d()
         self.kp = []
         self.des = []
+        
+    @classmethod
+    def load_from_file(cls, filename):
+        if (os.path.exists(filename)):
+            data = cv.imread(filename)
+            return cls(data)
+        else:
+            raise FileNotFoundError 
+
+    @classmethod
+    def load_from_array(cls, arr):
+        return cls(arr)
         
 
     def get_features(self, orb):
         self.kp, self.des = orb.detectAndCompute(self.image, None)
+
 
 class ImageMatch():
     def __init__(self, img1, img2, top_match_count=None):
@@ -51,8 +62,7 @@ class ImageMatch():
         else: match_count = top_match_count
         
         top_matches = self._get_top_matches(matches, match_count)
-        self.translation = self._get_translation(img1, img2, top_matches, match_count)
-        self._calc_offset(img1, img2, self.translation)
+        self.img2.translation = self._get_translation(img1, img2, top_matches, match_count)
 
     def _get_top_matches(self, matches, match_count):
         assert match_count <= len(matches)
@@ -91,32 +101,52 @@ class ImageMatch():
 
         return translation
 
-    def _calc_offset(self, img1, img2, translation):
-        if translation.x < 0:
-            img2.offset.x = 0
-            img1.offset.x = translation.x * -1
-        else:
-            img2.offset.x = translation.x
-            img1.offset.x = 0
-        if translation.y < 0:
-            img2.offset.y = 0
-            img1.offset.y = translation.y * -1
-        else:
-            img2.offset.y = translation.y
-            img1.offset.y = 0
 
+def stitch_images(images):
+    # find the size
+    max_translation_i = Vec2d()
+    min_translation_i = Vec2d()
+    
+    # all relative to first element
+    for i in range(1, len(images)):
+        # make relative translations absolution
+        x_translation = images[i].translation.x + images[i-1].translation.x
+        images[i].translation.x = x_translation
+        y_translation = images[i].translation.y + images[i-1].translation.y
+        images[i].translation.y = y_translation
+        if x_translation > images[max_translation_i.x].translation.x:
+            max_translation_i.x = i
+        elif x_translation < images[min_translation_i.x].translation.x:
+            min_translation_i.x = i
+        if y_translation > images[max_translation_i.y].translation.y:
+            max_translation_i.y = i
+        elif y_translation < images[min_translation_i.y].translation.y:
+            min_translation_i.y = i
+        print(x_translation, y_translation)
 
-def stitch_images(image_match):
-    img1 = image_match.img1
-    img2 = image_match.img2
-    blank_width = max(img1.image.shape[1], img2.image.shape[1] + image_match.translation.x) - min(0, image_match.translation.x)
-    blank_height = max(img1.image.shape[0], img2.image.shape[0] + image_match.translation.y) - min(0, image_match.translation.y)
-    blank_image_dimensions = Vec2d(blank_width, blank_height)
+    width = max(images[0].image.shape[1], images[max_translation_i.x].image.shape[1] + images[max_translation_i.x].translation.x) - min(0, images[min_translation_i.x].translation.x)
+    height = max(images[0].image.shape[0], images[max_translation_i.y].image.shape[0] + images[max_translation_i.y].translation.y) - min(0, images[min_translation_i.y].translation.y)
+    
+    blank_image_dimensions = Vec2d(width, height)
+    print(f"\nblank dimensions: {blank_image_dimensions}\n")
     blank_image = np.zeros((blank_image_dimensions.y, blank_image_dimensions.x, 3), np.uint8)
 
+    min_trans_x = images[min_translation_i.x].translation.x
+    min_trans_y = images[min_translation_i.y].translation.y
+    print("min translation: ", end="")
+    print(min_trans_x, min_trans_y)
+    i = 0
+    for img in images:
+        # print(img2.translation)
+        if min_trans_x < 0: img.offset.x = img.translation.x - min_trans_x
+        else: img.offset.x = img.translation.x
+        
+        if min_trans_y < 0: img.offset.y = img.translation.y - min_trans_y
+        else: img.offset.y = img.translation.y
+        
+        print(img.offset)
 
-    blank_image[img2.offset.y: img2.offset.y + img2.image.shape[0], img2.offset.x: img2.offset.x + img2.image.shape[1]] = img2.image
-    blank_image[img1.offset.y: img1.offset.y + img1.image.shape[0], img1.offset.x: img1.offset.x + img1.image.shape[1]] = img1.image
+        blank_image[img.offset.y: img.offset.y + img.image.shape[0], img.offset.x: img.offset.x + img.image.shape[1]] = img.image
 
     return blank_image
 
@@ -124,17 +154,24 @@ def stitch_images(image_match):
 
 if __name__ == "__main__":
     orb = cv.ORB_create()
+    images_directory = "sample_images"
+    files = sorted([f for f in os.listdir(images_directory) if f.endswith('.JPG')])
+    images = []
+    previous = BaseImage.load_from_file(os.path.join(images_directory, files[0]))
+    previous.get_features(orb)
+    images.append(previous)
+    # assume image i+1 overlaps i
+    for i in range(1, len(files)-1):
+        img2 = BaseImage.load_from_file(os.path.join(images_directory, files[i]))
+        img2.get_features(orb)
+        match = ImageMatch(previous, img2, 10)
+        images.append(img2)
+        previous = img2
 
-    img1 = BaseImage(os.path.join("sample_images", "right.png")) # queryImage / base image
-    img2 = BaseImage(os.path.join("sample_images", "left.png")) # trainImage / translated image
+        if i > 20:
+            break
 
-    img1.get_features(orb)
-    img2.get_features(orb)
-
-    image_match = ImageMatch(img1, img2)
-    combined_img = stitch_images(image_match)
-
-    # cv.imshow("output", combined_img)
-    # cv.waitKey(0)
-
-    cv.imwrite("output.png", combined_img)
+        
+    stitched_image = stitch_images(images)
+    resized = cv.resize(stitched_image, (0, 0), fx = 0.1, fy = 0.1)
+    cv.imwrite(os.path.join("result_images", f"output.png"), resized)
